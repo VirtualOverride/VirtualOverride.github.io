66892a19ef267c34f34aad3bf77323ab9776756a
Arjay Elbore <virtual@override.local>
2015-09-25 09:13:47 +0800

    add workflow media files

--- src/app/controllers/customers.controller.ts
+++ src/app/controllers/customers.controller.ts
@@ -1,8 +1,21 @@
 module demoApp {
     'use strict';
 
+    interface IWorkflowStage {
+        no: number;
+        name: string;
+    }
+
+    interface IWorkflowMedia {
+        image: string;
+        name: string;
+        date: string;
+    }
+
     class CustomersController {
         workflows: ITreeNode[] = null;
+        workflowStages: IWorkflowStage[] = null;
+        workflowMedias: IWorkflowMedia[] = null;
 
         static $inject = ['demoApp.dataService'];
 
@@ -12,6 +25,17 @@ module demoApp {
                 .then((results: ITreeNode[]) => {
                     this.workflows = results;
                 });
+
+            this.workflowStages = [
+                { no: 1, name: 'Workflow Name 1' },
+                { no: 2, name: 'Workflow Name 2' }
+            ];
+
+            this.workflowMedias = [
+                { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' },
+                { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' },
+                { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' }
+            ];
         }
     }
 
--- src/js/controllers/customers.controller.js
+++ src/js/controllers/customers.controller.js
@@ -5,11 +5,22 @@ var demoApp;
         function CustomersController(dataService) {
             var _this = this;
             this.workflows = null;
+            this.workflowStages = null;
+            this.workflowMedias = null;
             dataService
                 .getTreeNode()
                 .then(function (results) {
                 _this.workflows = results;
             });
+            this.workflowStages = [
+                { no: 1, name: 'Workflow Name 1' },
+                { no: 2, name: 'Workflow Name 2' }
+            ];
+            this.workflowMedias = [
+                { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' },
+                { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' },
+                { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' }
+            ];
         }
         CustomersController.$inject = ['demoApp.dataService'];
         return CustomersController;
