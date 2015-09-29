1a57b45d40a394846047c15f7798db002e7439c4
virtualoverride <b.yajra.eroble@gmail.com>
2015-09-29 16:53:40 +0800

    project dashboard and collaboration

--- src/app/components/project.collaboration/project.collaboration.controller.ts
+++ src/app/components/project.collaboration/project.collaboration.controller.ts
@@ -1,10 +1,29 @@
 module demoApp {
 	'use strict';
+
+	interface IClient {
+		id: number;
+		name: string;		
+	}
+
+	interface IProjectWorkflowClient extends IClient {
+		id: number;
+		name: string;
+	}
+	
+	interface IMember extends IClient {
+		email: string;
+		role: string;
+	}
 	
 	class ProjectCollaborationController {
 		
 		workflowMedias: IWorkflowMedia[] = null;
 		workflowStages: IWorkflowStage[] = null;
+		project_clients: IProjectWorkflowClient[] = null;
+		members: IMember[] = null;
+
+		selected_client: string = null;
 		
 		constructor(){
 			
@@ -15,10 +34,19 @@ module demoApp {
             ];
 			
             this.workflowStages = [
-                { no: 1, name: 'Workflow Name 1' },
-                { no: 2, name: 'Workflow Name 2' }
+                { no: 1, name: 'Workflow stage name 1' },
+                { no: 2, name: 'Workflow stage name 2' }
             ];
 			
+			this.project_clients = [
+				{ id: 1, name: 'Jack' },
+				{ id: 2, name: "Richard"}
+			];
+			
+			this.members = [
+				{ id: 1, name: "Jack Daniels", email: "jack@daniels.com", role: "Administrator" },
+				{ id: 2, name: "Richard Hennessy", email: "richard@hennessy.com", role: "Administrator" }		
+			];
 		}
 		
 	}
--- src/js/components/project.collaboration/project.collaboration.controller.js
+++ src/js/components/project.collaboration/project.collaboration.controller.js
@@ -5,14 +5,25 @@ var demoApp;
         function ProjectCollaborationController() {
             this.workflowMedias = null;
             this.workflowStages = null;
+            this.project_clients = null;
+            this.members = null;
+            this.selected_client = null;
             this.workflowMedias = [
                 { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' },
                 { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' },
                 { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' }
             ];
             this.workflowStages = [
-                { no: 1, name: 'Workflow Name 1' },
-                { no: 2, name: 'Workflow Name 2' }
+                { no: 1, name: 'Workflow stage name 1' },
+                { no: 2, name: 'Workflow stage name 2' }
+            ];
+            this.project_clients = [
+                { id: 1, name: 'Jack' },
+                { id: 2, name: "Richard" }
+            ];
+            this.members = [
+                { id: 1, name: "Jack Daniels", email: "jack@daniels.com", role: "Administrator" },
+                { id: 2, name: "Richard Hennessy", email: "richard@hennessy.com", role: "Administrator" }
             ];
         }
         return ProjectCollaborationController;
