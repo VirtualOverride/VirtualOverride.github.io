2b35d3d14556f2b040e0ed9dbfd229ae3c89e855
Arjay Elbore <b.yajra.eroble@gmail.com>
2015-10-09 11:27:13 +0800

    updates

--- src/app/components/dashboards/workflow/workflow.controller.ts
+++ src/app/components/dashboards/workflow/workflow.controller.ts
@@ -1,9 +1,22 @@
 module demoApp {
 	'use strict';
+    
+    interface IAccordionContent {
+        header: string;
+        is_open: boolean;
+        template: string;
+    }
 		
 	class WorkflowDashboard {
 		workflows: ITreeNode[] = null;
         workflow_stages: IWorkflowStage[] = null;
+        description: string = null; 
+        accordion_contents: IAccordionContent[] = null;
+      
+        workflowFormStandards: IForm[] = null;
+        workflowFormAdvances: IForm[] = null;
+        
+        workflowMedias: IWorkflowMedia[] = null;
          
 		static $inject = ['demoApp.dataService']; 
 		
@@ -15,10 +28,37 @@ module demoApp {
                 });			
                 
              this.workflow_stages = [
-                { no: 1, name: 'Workflow Name 1' },
+                { no: 1, name: 'Workflow Name 1' }, 
                 { no: 2, name: 'Workflow Name 2' }
             ];               
             
+            this.description = 'This is a sample description';
+            
+            this.accordion_contents = [
+                { header: 'Description', is_open: true, template: 'app/components/dashboards/workflow/workflow-stage-content-description.html' },
+                { header: 'Form', is_open: true, template: 'app/components/dashboards/workflow/workflow-stage-content-form.html' },
+                { header: 'Media', is_open: true, template: 'app/components/dashboards/workflow/workflow-stage-content-media.html'}
+            ];
+            
+            this.workflowFormStandards = [
+                { id: 1, text: 'Title' },
+                { id: 2, text: 'Text field' },
+                { id: 3, text: 'Checkbox' },
+                { id: 4, text: 'Radio buttons' }              
+            ];
+
+            this.workflowFormAdvances = [
+                { id: 1, text: 'Date' },
+                { id: 2, text: 'Url' },
+                { id: 3, text: 'File Upload' },
+                { id: 4, text: 'Email' } 
+            ];
+        
+             this.workflowMedias = [
+                { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' },
+                { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' },
+                { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' }
+            ];        
 		}		
 	}
 	
--- src/js/components/dashboards/workflow/workflow.controller.js
+++ src/js/components/dashboards/workflow/workflow.controller.js
@@ -7,6 +7,11 @@ var demoApp;
             this.dataService = dataService;
             this.workflows = null;
             this.workflow_stages = null;
+            this.description = null;
+            this.accordion_contents = null;
+            this.workflowFormStandards = null;
+            this.workflowFormAdvances = null;
+            this.workflowMedias = null;
             dataService
                 .getTreeNode()
                 .then(function (results) {
@@ -15,6 +20,29 @@ var demoApp;
             this.workflow_stages = [
                 { no: 1, name: 'Workflow Name 1' },
                 { no: 2, name: 'Workflow Name 2' }
+            ];
+            this.description = 'This is a sample description';
+            this.accordion_contents = [
+                { header: 'Description', is_open: true, template: 'app/components/dashboards/workflow/workflow-stage-content-description.html' },
+                { header: 'Form', is_open: true, template: 'app/components/dashboards/workflow/workflow-stage-content-form.html' },
+                { header: 'Media', is_open: true, template: 'app/components/dashboards/workflow/workflow-stage-content-media.html' }
+            ];
+            this.workflowFormStandards = [
+                { id: 1, text: 'Title' },
+                { id: 2, text: 'Text field' },
+                { id: 3, text: 'Checkbox' },
+                { id: 4, text: 'Radio buttons' }
+            ];
+            this.workflowFormAdvances = [
+                { id: 1, text: 'Date' },
+                { id: 2, text: 'Url' },
+                { id: 3, text: 'File Upload' },
+                { id: 4, text: 'Email' }
+            ];
+            this.workflowMedias = [
+                { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' },
+                { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' },
+                { image: 'assets/images/u70.png', name: 'name.file_format', date: 'dd/mm/yyyy' }
             ];
         }
         WorkflowDashboard.$inject = ['demoApp.dataService'];
